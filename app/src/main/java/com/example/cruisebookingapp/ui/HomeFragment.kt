7fbd798d0bef7f85b40af0958ee969637bb1146c
Samora Machel <samora@me.laptop>
2022-04-29 00:13:38 +0300

    Connected the search adapter with the home view

--- app/src/main/java/com/example/cruisebookingapp/ui/HomeFragment.kt
+++ app/src/main/java/com/example/cruisebookingapp/ui/HomeFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
@@ -14,6 +15,7 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.cruisebookingapp.R
 import com.example.cruisebookingapp.ui.adapter.HomeFragmentAdapter
+import com.example.cruisebookingapp.ui.adapter.SearchAdapter
 import com.example.cruisebookingapp.ui.viewmodel.HomeFragmentViewModel
 
 
@@ -31,14 +33,39 @@ class HomeFragment : Fragment() {
         val mainView = inflater.inflate(R.layout.fragment_home, container, false)
 
         val viewModel = ViewModelProvider(this).get(HomeFragmentViewModel::class.java)
+        val searchView : SearchView = mainView.findViewById(R.id.shipSearchView)
 
+        val shipRecycler : RecyclerView =  mainView.findViewById(R.id.shipRecyclerView)
+        val homeFragmentManager = this.requireFragmentManager()
 
         // setup the recycler view
         viewModel.getShipList(requireContext()).observe(viewLifecycleOwner, Observer {
-            val shipRecycler : RecyclerView =  mainView.findViewById(R.id.shipRecyclerView)
-            shipRecycler.adapter = HomeFragmentAdapter(it, this.requireFragmentManager())
+            shipRecycler.adapter = HomeFragmentAdapter(it, homeFragmentManager)
             shipRecycler.layoutManager =  GridLayoutManager(requireContext(), resources.getInteger(R.integer.home_grid_span)) //StaggeredGridLayoutManager(R.integer.home_grid_span, StaggeredGridLayoutManager.VERTICAL)
+
+
+            searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(p0: String?): Boolean {
+                    shipRecycler.adapter = p0?.let { it1 ->
+                        SearchAdapter(it, homeFragmentManager).search(it1)
+                    }
+
+                    shipRecycler.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+                    return true
+                }
+
+                override fun onQueryTextChange(p0: String?): Boolean {
+                    shipRecycler.adapter = p0?.let { it1 ->
+                        SearchAdapter(it, homeFragmentManager).search(it1)
+                    }
+
+                    shipRecycler.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+                    return true
+                }
+
+            })
         })
+
 
 
 
--- app/src/main/java/com/example/cruisebookingapp/ui/adapter/SearchAdapter.kt
+++ app/src/main/java/com/example/cruisebookingapp/ui/adapter/SearchAdapter.kt
@@ -6,17 +6,24 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.cruisebookingapp.BottomFragment
 import com.example.cruisebookingapp.R
 import com.example.cruisebookingapp.data.model.Ship
 
 class SearchAdapter(val shipList : List<Ship>, val fragmentManager : FragmentManager) : RecyclerView.Adapter<SearchAdapter.SearchViewHolder>(){
 
-    private val searchList : MutableList<Ship> = shipList as MutableList<Ship>
+    private val searchList : MutableList<Ship> = mutableListOf()
+
+    init {
+        searchList.addAll(shipList)
+    }
 
     class SearchViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView) {
+        val shipCard : CardView = itemView.findViewById(R.id.searchShipCard)
         val shipImage : ImageView = itemView.findViewById(R.id.searchShipImage)
         val shipName : TextView = itemView.findViewById(R.id.searchShipName)
         val shipType : TextView = itemView.findViewById(R.id.searchShipType)
@@ -33,27 +40,38 @@ class SearchAdapter(val shipList : List<Ship>, val fragmentManager : FragmentMan
 
         Glide.with(holder.context)
             .load(_ship.image)
+            .error(R.drawable.holder)
             .into(holder.shipImage)
 
         holder.shipName.text = _ship.ship_name
         holder.shipType.text = _ship.ship_type
+
+        holder.shipCard.setOnClickListener {
+            val bottomFrag = BottomFragment.newInstance(_ship.ship_id)
+            bottomFrag.show(fragmentManager, BottomFragment.toString())
+        }
     }
 
     override fun getItemCount(): Int {
         return searchList.size
     }
 
-    fun search(value : String) {
+    fun search(value : String): SearchAdapter {
         searchList.clear()
-        for(ship in shipList) {
-            if (ship.ship_name.contains(value))
-                searchList.add(ship)
-            else if(ship.ship_type.contains(value))
-                searchList.add(ship)
-            else if(ship.ship_model?.contains(value) == true)
-                searchList.add(ship)
+        if(value != "" ) {
+            for(ship in shipList) {
+                if (ship.ship_name.contains(value))
+                    searchList.add(ship)
+                else if(ship.ship_type.contains(value))
+                    searchList.add(ship)
+                else if(ship.ship_model?.contains(value) == true)
+                    searchList.add(ship)
+            }
+        } else {
+            searchList.addAll(shipList)
         }
         notifyDataSetChanged()
+        return this
     }
 
 }
